8133ffb341752234f59e8915c9c14bf4781762a0
Panigale <davy123498@gmail.com>
2019-03-28 19:58:35 +0800

    add asyncData support

--- src/entry-client.js
+++ src/entry-client.js
@@ -1,6 +1,59 @@
 
+import Vue from 'vue';
 import createApp from './main';
+import ProgressBar from './components/ProgressBar.vue';
 
-const app = createApp();
+const bar = Vue.prototype.$bar = new Vue(ProgressBar).$mount()
+document.body.appendChild(bar.$el)
 
-app.$mount('#app');
+Vue.mixin({
+    beforeRouteUpdate(to, from, next) {
+        const { asyncData } = this.$options
+        if (asyncData) {
+            asyncData({
+                store: this.$store,
+                route: to
+            }).then(next).catch(next)
+        } else {
+            next()
+        }
+    }
+})
+
+const { app, router ,store } = createApp();
+
+if (window.__INITIAL_STATE__) {
+  store.replaceState(window.__INITIAL_STATE__)
+}
+
+router.onReady(() => {
+    // Add router hook for handling asyncData.
+    // Doing it after initial route is resolved so that we don't double-fetch
+    // the data that we already have. Using router.beforeResolve() so that all
+    // async components are resolved.
+    router.beforeResolve((to, from, next) => {
+        const matched = router.getMatchedComponents(to)
+        const prevMatched = router.getMatchedComponents(from)
+
+        
+        let diffed = false
+        const activated = matched.filter((c, i) => {
+            return diffed || (diffed = (prevMatched[i] !== c))
+        })
+        const asyncDataHooks = activated.map(c => c.asyncData).filter(_ => _)
+        if (!asyncDataHooks.length) {
+            return next()
+        }
+
+        bar.start()
+        Promise.all(asyncDataHooks.map(hook => hook({ store, route: to })))
+            .then(() => {
+                bar.finish()
+                next()
+            })
+            .catch(next)
+    })
+
+    app.$mount('#app');
+
+})
